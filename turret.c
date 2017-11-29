935bab78a60d343569e96a0a189903b08dce296f
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-11-29 07:51:08 -0800

    Update turret.c

--- turret.c
+++ turret.c
@@ -19,8 +19,8 @@ static unsigned int gpio_fire = 32;
 module_param(gpio_fire,uint,S_IRUGO);
 MODULE_PARM_DESC(gpio_fire, " GPIO LED number (default=32)");     ///< parameter description
 
-enum modes { TURN, RAISE, FIRE, STANDBY};
-static enum modes mode = STANDBY;
+//enum modes { TURN, RAISE, FIRE, STANDBY};
+//static enum modes mode = STANDBY;
 
 static int __init turret_init(void) {
   gpio_request(gpio_fire,"sysfs");
