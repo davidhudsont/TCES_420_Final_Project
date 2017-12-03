66a9d6ba17d0a4631f96b3c84fe650bc057882f0
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2017-12-02 21:51:45 -0800

    Update turret.c

--- turret.c
+++ turret.c
@@ -65,72 +65,72 @@ MODULE_PARM_DESC(rotation_v, " Firing All Interface To fire all missiles set to 
 
 
 static ssize_t NR_MISSILES_REMAINING(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf,"Number of missiles remaining: %d\n", nr_missiles);
+	return sprintf(buf,"Number of missiles remaining: %d\n", nr_missiles);
 }
 
 static ssize_t SET_NR_MISSILES(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    mutex_lock(&nr_missiles_lock);
+	mutex_lock(&nr_missiles_lock);
 	unsigned int amount;
-    sscanf(buf,"%du",&amount);
-    if ((amount>=0)&&(amount <=4)){
-        nr_missiles = amount;
-    } 
+	sscanf(buf,"%du",&amount);
+	if ((amount>=0)&&(amount <=4)){
+		nr_missiles = amount;
+	} 
 	mutex_unlock(&nr_missiles_lock);
-    return count;
+	return count;
 }
 
 static ssize_t FIRE_ONE_SHOW(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf,"FIRE ONE STATE: %d\n", FIRE_ONE);
+	return sprintf(buf,"FIRE ONE STATE: %d\n", FIRE_ONE);
 }
 
 static ssize_t FIRE_ONE_SET(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    mutex_lock(&fire_one_lock);
+	mutex_lock(&fire_one_lock);
 	unsigned int amount;
-    sscanf(buf,"%du",&amount);
-    if (amount == 1){
-        FIRE_ONE = amount;
-    } 
+	sscanf(buf,"%du",&amount);
+	if (amount == 1){
+		FIRE_ONE = amount;
+	} 
 	mutex_unlock(&fire_one_lock);
-    return count;
+	return count;
 }
 
 static ssize_t FIRE_ALL_SHOW(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf,"FIRE ALL STATE: %d\n", FIRE_ONE);
+	return sprintf(buf,"FIRE ALL STATE: %d\n", FIRE_ONE);
 }
 
 static ssize_t FIRE_ALL_SET(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    mutex_lock(&fire_all_lock);
+	mutex_lock(&fire_all_lock);
 	unsigned int amount;
-    sscanf(buf,"%du",&amount);
-    if (amount == 1){
-        FIRE_ALL = amount;
-    } 
+	sscanf(buf,"%du",&amount);
+	if (amount == 1){
+		FIRE_ALL = amount;
+	} 
 	mutex_unlock(&fire_all_lock);
-    return count;
+	return count;
 }
 static ssize_t ROTATE_H_SHOW(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf,"Rotate H STATE: %d\n", rotation_h);
+    	return sprintf(buf,"Rotate H STATE: %d\n", rotation_h);
 }
 
 static ssize_t ROTATE_H_SET(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    mutex_lock(&rotation_h_lock);
+	mutex_lock(&rotation_h_lock);
 	int amount;
-    sscanf(buf,"%d",&amount);
+	sscanf(buf,"%d",&amount);
 	rotation_h = amount;
 	mutex_unlock(&rotation_h_lock);
-    return count;
+	return count;
 }
 static ssize_t ROTATE_V_SHOW(struct kobject *kobj, struct kobj_attribute *attr, char *buf){
-    return sprintf(buf,"Rotate V STATE: %d\n", rotation_v);
+	return sprintf(buf,"Rotate V STATE: %d\n", rotation_v);
 }
 
 static ssize_t ROTATE_V_SET(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count){
-    mutex_lock(&rotation_v_lock);
+	mutex_lock(&rotation_v_lock);
 	int amount;    	
 	sscanf(buf,"%d",&amount); 
 	rotation_v = amount;
 	mutex_unlock(&rotation_v_lock);
-    return count;
+	return count;
 }
 static struct kobj_attribute nr_missile_attr = __ATTR(nr_missile,(S_IWUSR|S_IRUGO),NR_MISSILES_REMAINING,SET_NR_MISSILES);
 static struct kobj_attribute fire_one_attr = __ATTR(FIRE_ONE,(S_IWUSR|S_IRUGO),FIRE_ONE_SHOW,FIRE_ONE_SET);
@@ -139,18 +139,18 @@ static struct kobj_attribute rotate_h_attr = __ATTR(rotation_h,(S_IWUSR|S_IRUGO)
 static struct kobj_attribute rotate_v_attr = __ATTR(rotation_v,(S_IWUSR|S_IRUGO),ROTATE_V_SHOW,ROTATE_V_SET);
 
 static struct attribute *pi_attrs[] = {
-    &nr_missile_attr.attr,
-    &fire_one_attr.attr,
-    &fire_all_attr.attr,
+	&nr_missile_attr.attr,
+	&fire_one_attr.attr,
+	&fire_all_attr.attr,
 	&rotate_h_attr.attr,
 	&rotate_v_attr.attr,
-    NULL,  
+	NULL,  
 };
 
 static char tname[7] = "Turret";
 static struct attribute_group attr_group = {
-    .name = tname,
-    .attrs = pi_attrs,
+	.name = tname,
+	.attrs = pi_attrs,
 };
 
 static struct kobject *pi_kobj;
@@ -159,43 +159,43 @@ static struct task_struct *task_rotate_h;
 static struct task_struct *task_rotate_v;
 
 static int FIRING(void *arg) {
-    printk(KERN_INFO "Turret Firing: Thread has started running\n");
-    while(!kthread_should_stop()){
+	printk(KERN_INFO "Turret Firing: Thread has started running\n");
+	while(!kthread_should_stop()){
 		mutex_lock(&firing_lock);
-        set_current_state(TASK_RUNNING);
-        if ((FIRE_ONE == 1) && (nr_missiles > 0)) {
-            gpio_set_value(gpio_fire,FIRE_ONE);
-            ssleep(6);
+		set_current_state(TASK_RUNNING);
+		if ((FIRE_ONE == 1) && (nr_missiles > 0)) {
+			gpio_set_value(gpio_fire,FIRE_ONE);
+			ssleep(6);
 			mutex_lock(&nr_missiles_lock);
-            nr_missiles--;
+			nr_missiles--;
 			mutex_unlock(&nr_missiles_lock);
 			mutex_lock(&fire_one_lock);
-            FIRE_ONE = 0;
+			FIRE_ONE = 0;
 			mutex_unlock(&fire_one_lock);
-            gpio_set_value(gpio_fire,FIRE_ONE);
-        }
-        if ((FIRE_ALL == 1) &&( nr_missiles > 0)) {
-            gpio_set_value(gpio_fire,FIRE_ALL);
-            ssleep(6*nr_missiles);
+			gpio_set_value(gpio_fire,FIRE_ONE);
+		}
+		if ((FIRE_ALL == 1) &&( nr_missiles > 0)) {
+			gpio_set_value(gpio_fire,FIRE_ALL);
+			ssleep(6*nr_missiles);
 			mutex_lock(&nr_missiles_lock);
-            nr_missiles = 0;
+			nr_missiles = 0;
 			mutex_unlock(&nr_missiles_lock);
 			mutex_lock(&fire_all_lock);
-            FIRE_ALL = 0;
+			FIRE_ALL = 0;
 			mutex_unlock(&fire_all_lock);
-            gpio_set_value(gpio_fire,FIRE_ALL);
-        }
+			gpio_set_value(gpio_fire,FIRE_ALL);
+		}
 		mutex_unlock(&firing_lock);
 		set_current_state(TASK_INTERRUPTIBLE);
-    }
-    printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
+	}
+	printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
     return 0;
 }
 static int Rotation_H(void *arg) {
-    printk(KERN_INFO "Turret Firing: Thread has started running\n");
-    while(!kthread_should_stop()){
+	printk(KERN_INFO "Turret Firing: Thread has started running\n");
+	while(!kthread_should_stop()){
 		mutex_lock(&firing_lock);
-        set_current_state(TASK_RUNNING);
+		set_current_state(TASK_RUNNING);
 		if (rotation_h > 0) {
 			gpio_set_value(gpio_turn_c,true);
 			msleep(rotation_h);
@@ -215,16 +215,16 @@ static int Rotation_H(void *arg) {
 		}
 		mutex_unlock(&firing_lock);
 		set_current_state(TASK_INTERRUPTIBLE);
-    }
-    printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
-    return 0;
+	}
+	printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
+	return 0;
 }
 
 static int Rotation_V(void *arg) {
-    printk(KERN_INFO "Turret Firing: Thread has started running\n");
-    while(!kthread_should_stop()){
+	printk(KERN_INFO "Turret Firing: Thread has started running\n");
+	while(!kthread_should_stop()){
 		mutex_lock(&firing_lock);
-        set_current_state(TASK_RUNNING);
+		set_current_state(TASK_RUNNING);
 		if (rotation_v > 0) {
 			gpio_set_value(gpio_raise_turret,true);
 			msleep(rotation_v);
@@ -244,9 +244,9 @@ static int Rotation_V(void *arg) {
 		}
 		mutex_unlock(&firing_lock);
 		set_current_state(TASK_INTERRUPTIBLE);
-    }
-    printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
-    return 0;
+	}
+	printk(KERN_INFO "Turret Firing: Thread has run to completion\n");
+	return 0;
 }
 
 static int __init turret_init(void) {
@@ -256,78 +256,78 @@ static int __init turret_init(void) {
 	mutex_init(&fire_all_lock);
 	mutex_init(&rotation_h_lock);
 	int result = 0;
-    pi_kobj = kobject_create_and_add("pi", kernel_kobj->parent);
-    if (!pi_kobj){
-        printk(KERN_ALERT "Failed to create kobject\n");
-        kobject_put(pi_kobj);
-        return -ENOMEM;
-    }
-    result = sysfs_create_group(pi_kobj, &attr_group);
-    if (result){
-        printk(KERN_ALERT "Turret : failed to create sysfs group\n");
-        kobject_put(pi_kobj);
-        return result;
-    }
-    gpio_request(gpio_fire,"sysfs");
-    gpio_direction_output(gpio_fire,false);
-    gpio_export(gpio_fire,false);
-	
+	pi_kobj = kobject_create_and_add("pi", kernel_kobj->parent);
+	if (!pi_kobj){
+	printk(KERN_ALERT "Failed to create kobject\n");
+	kobject_put(pi_kobj);
+	return -ENOMEM;
+	}
+	result = sysfs_create_group(pi_kobj, &attr_group);
+	if (result){
+	printk(KERN_ALERT "Turret : failed to create sysfs group\n");
+	kobject_put(pi_kobj);
+	return result;
+	}
+	gpio_request(gpio_fire,"sysfs");
+	gpio_direction_output(gpio_fire,false);
+	gpio_export(gpio_fire,false);
+
 	gpio_request(gpio_turn_c,"sysfs");
-    gpio_direction_output(gpio_turn_c,false);
-    gpio_export(gpio_turn_c,false);
-	
+	gpio_direction_output(gpio_turn_c,false);
+	gpio_export(gpio_turn_c,false);
+
 	gpio_request(gpio_turn_cc,"sysfs");
-    gpio_direction_output(gpio_turn_cc,false);
-    gpio_export(gpio_turn_cc,false);
-	
+	gpio_direction_output(gpio_turn_cc,false);
+	gpio_export(gpio_turn_cc,false);
+
 	gpio_request(gpio_raise_turret,"sysfs");
-    gpio_direction_output(gpio_raise_turret,false);
-    gpio_export(gpio_raise_turret,false);
-	
+	gpio_direction_output(gpio_raise_turret,false);
+	gpio_export(gpio_raise_turret,false);
+
 	gpio_request(gpio_lower_turret,"sysfs");
-    gpio_direction_output(gpio_lower_turret,false);
-    gpio_export(gpio_lower_turret,false);
-	
-	
-    task_fire = kthread_run(FIRING, NULL, "FIRE_THREAD");
+	gpio_direction_output(gpio_lower_turret,false);
+	gpio_export(gpio_lower_turret,false);
+
+
+	task_fire = kthread_run(FIRING, NULL, "FIRE_THREAD");
 	task_rotate_h = kthread_run(Rotation_H,NULL,"Rotation_H_Thread");
 	task_rotate_v = kthread_run(Rotation_V,NULL,"Rotation_V_Thread");
-	
-    if (IS_ERR((task_fire) || (task_rotate_h)||(task_rotate_v))) {
-        printk(KERN_ALERT "Turret: Failed to create the task\n");
-        return PTR_ERR(task_fire);
-    }
-    printk("<1> Loading Turret Module\n");
-    return result;
+
+	if (IS_ERR((task_fire) || (task_rotate_h)||(task_rotate_v))) {
+	printk(KERN_ALERT "Turret: Failed to create the task\n");
+	return PTR_ERR(task_fire);
+	}
+	printk("<1> Loading Turret Module\n");
+	return result;
 }
 
 
 
 static void __exit turret_exit(void) {
-    kthread_stop(task_fire);
+   	kthread_stop(task_fire);
 	kthread_strop(task_rotate_h);
 	kthread_strop(task_rotate_v);
 	
-    kobject_put(pi_kobj);
-    gpio_set_value(gpio_fire,0);
+    	kobject_put(pi_kobj);
+    	gpio_set_value(gpio_fire,0);
 	gpio_set_value(gpio_turn_c,0);
 	gpio_set_value(gpio_turn_cc,0);
 	gpio_set_value(gpio_raise_turret,0);
 	gpio_set_value(gpio_lower_turret,0);
 	
-    gpio_unexport(gpio_fire);
+    	gpio_unexport(gpio_fire);
 	gpio_unexport(gpio_turn_c);
 	gpio_unexport(gpio_turn_cc);
 	gpio_unexport(gpio_raise_turret);
 	gpio_unexport(gpio_lower_turret);
 	
-    gpio_free(gpio_fire);
+    	gpio_free(gpio_fire);
 	gpio_free(gpio_turn_c);
 	gpio_free(gpio_turn_cc);
 	gpio_free(gpio_raise_turret);
 	gpio_free(gpio_lower_turret);
 	
-    printk(KERN_INFO "Removing Turret Module\n");
+    	printk(KERN_INFO "Removing Turret Module\n");
 }
 
 module_init(turret_init);
